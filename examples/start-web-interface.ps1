39e8b9a8429b16eefdfcbaae183647056c822ef4
Putra Budiman <cristminix@gmail.com>
2025-09-27 18:34:19 +0700

    ...

--- examples/start-web-interface.ps1
+++ examples/start-web-interface.ps1
@@ -7,4 +7,6 @@ $env:PYTHONPATH = "$(Get-Location);$(Get-Location)/.deps/gpt4free"
 #$env:G4F_PROXY = "socks5://127.0.0.1:1081"
  $env:G4F_PROXY = ""
 # Run the web interface
-uv run python -m examples.custom_inference_api.web_interface --debug
+# uv run python -m examples.custom_inference_api.web_interface --debug
+
+uv run uvicorn examples.solids.extended.run_custom_gui:app --host 0.0.0.0 --port 7000 --reload
